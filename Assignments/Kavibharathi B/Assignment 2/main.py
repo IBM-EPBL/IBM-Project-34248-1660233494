266f59dc1cd98dbcff0f80d00a4669e1dbc5bde1
MaamathiK <81421160+MaamathiK@users.noreply.github.com>
2022-10-14 15:50:58 +0530

    Add files via upload

---
+++ Assignments/Kavibharathi B/Assignment 2/main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/home')
+def ho():
+    return render_template("frontpage.html")
+@app.route('/abo')
+def about():
+    return render_template("abo.html")
+if __name__ == '__main__':
+    app.run(debug=True)
